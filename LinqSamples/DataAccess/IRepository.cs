ef4672b9587c5fc171ce344d0ec951c3f11f48ed
Andrey Vronskiy <rainbow191991@gmail.com>
2021-05-05 13:02:29 +0300

    added LINQ

--- LinqSamples/DataAccess/IRepository.cs
+++ LinqSamples/DataAccess/IRepository.cs
@@ -1,14 +1,129 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+
 
 namespace AnalyticsAdapter
 {
+    public class Repository : IRepository
+    {
+        private readonly Database _db;
+
+        public Repository(Database db)
+        {
+            _db = db;                                                                                                                                                                                                                        ;
+        }
+        public Order[] GetOrders(int customerId)
+        {
+           return _db.Orders
+                .Where(order => order.CustomerId == customerId)
+                .ToArray();
+        }
+
+        public Order GetOrder(int orderId)
+        {
+            var order= _db.Orders.SingleOrDefault(order => order.Id == orderId);
+            if(order == null)
+            {
+                throw new InvalidOperationException();
+            }
+            return order;
+        }
+
+         public decimal GetMoneySpentBy(int customerId) 
+        {
+            return _db.Orders.Join(_db.Products,
+                (o) => o.ProductId,
+                (p)=>p.Id,
+
+                (o, p) => new
+                {
+                    p.Price,
+                    o.CustomerId,
+                })
+                .Where(x=>x.CustomerId==customerId)
+                .Sum(x=>x.Price);
+        }
+        public CustomerOverview GetCustomerOverview(int customerId)
+        {
+            var name = _db.Customers.Single(x => x.Id == customerId).Name;
+            return new CustomerOverview
+            {
+                Name = name,
+                TotalMoneySpent = GetMoneySpentBy(customerId),
+                FavoriteProductName = GetFavoriteProductName(customerId)
+            };
+        }
+
+        private string GetFavoriteProductName(int customerId)
+        {
+            var productId = GetOrders(customerId).Join(_db.Products,
+                (o) => o.ProductId,
+                (p) => p.Id,
+                (o, p) => new
+                {
+                    ProductId = p.Id,
+                    ProductName = p.Name,
+
+                })
+                .GroupBy(x => x.ProductId)
+                .Select(g => new
+                {
+                    ProductId = g.Key,
+                    Count = g.Count(),
+                    ProductName=g.First().ProductName
+                })
+                .OrderBy(x => x.Count)
+                .Last()
+                .ProductId;
+            return _db.Products.Single(x => x.Id == productId).Name;
+        }
+
+        public bool AreAllPurchasesHigherThan(int customerId, decimal targetPrice)
+        {
+            return Orders.Where(order => order.CustomerId == customerId)
+                 .Join(Products,
+                     order => order.ProductId,
+                     product => product.Id,
+                     (order, product) => product)
+                 .All(product => product.Price > targetPrice);
+        }
+
+        public Product[] GetAllProductsPurchased(int customerId)
+        {
+            return Orders.Where(order => order.CustomerId == customerId)
+                .Join(Products,
+                    order => order.ProductId,
+                    product => product.Id,
+                    (order, product) => product).ToArray();
+        }
+        public Product[] GetUniqueProductsPurchased(int customerId)
+        {
+            return Orders.Where(order => order.CustomerId == customerId)
+                .Select(order => order.ProductId)
+                .Distinct()
+                .Join(Products,
+                    id => id,
+                    product => product.Id,
+                    (id, product) => product).ToArray();
+        }
+        public int GetTotalProductsPurchased(int productId)
+        {
+            return Orders.Count(order => order.ProductId == productId);
+        }
+
+        public bool HasEverPurchasedProduct(int customerId, int productId)
+        {
+            return Orders.Any(order => order.CustomerId == customerId && order.ProductId == productId);
+        }
+    }
     public interface IRepository
     {
-        Order[] GetOrders(int customerId);
+        Order[] GetOrders(int customerId);//
 
-        Order GetOrder(int orderId);
+        Order GetOrder(int orderId);//
 
-        decimal GetMoneySpentBy(int customerId);
+        decimal GetMoneySpentBy(int customerId);//
 
         Product[] GetAllProductsPurchased(int customerId);
 
@@ -22,7 +137,7 @@ namespace AnalyticsAdapter
 
         bool DidPurchaseAllProducts(int customerId, params int[] productIds);
 
-        CustomerOverview GetCustomerOverview(int customerId);
+        CustomerOverview GetCustomerOverview(int customerId);//
 
         List<(string productName, int numberOfPurchases)> GetProductsPurchased(int customerId);
     }
