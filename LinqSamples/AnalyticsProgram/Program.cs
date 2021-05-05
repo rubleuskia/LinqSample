1e603ba712c187a8298aef0791857d1ae8da9dbb
Aleksey Rublevsky <sweexxik@gmail.com>
2021-05-05 22:08:16 +0300

    Job scheduler classwork

--- LinqSamples/AnalyticsProgram/Program.cs
+++ LinqSamples/AnalyticsProgram/Program.cs
@@ -1,4 +1,8 @@
-﻿using AnalyticsAdapter;
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Text;
 
 namespace AnalyticsProgram
 {
@@ -6,9 +10,49 @@ namespace AnalyticsProgram
     {
         static void Main(string[] args)
         {
-            var adapter = new AnalyticsAdapter();
-            var db = new Database();
-            adapter.Connect(new Repository(db));
+            var scheduler = new JobScheduler.JobScheduler(5000);
+
+            scheduler.AddHandler(LogExecutionTimeInConsole);
+            scheduler.AddHandler(LogExecutionTimeInFile);
+            scheduler.AddHandler((_) => DownloadWebsite());
+
+            scheduler.Start();
+
+            Console.ReadKey();
+        }
+
+        private static void LogExecutionTimeInConsole(DateTime signalTime)
+        {
+            Console.WriteLine($"Executed: {signalTime}");
+        }
+
+        private static void LogExecutionTimeInFile(DateTime signalTime)
+        {
+            WriteToFile("ExecutionTimeLog.txt", signalTime.ToString(CultureInfo.InvariantCulture));
+        }
+
+        private static void DownloadWebsite()
+        {
+            WebClient client = new WebClient();
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+            string reply = client.DownloadString("https://stackoverflow.com/questions/26233/fastest-c-sharp-code-to-download-a-web-page");
+
+            WriteToFile("Stackoverflow.txt", reply);
+        }
+
+        private static void WriteToFile(string path, string text)
+        {
+            if (!File.Exists(path))
+            {
+                using var stream = File.Create(path);
+                byte[] info = new UTF8Encoding(true).GetBytes(text);
+                stream.Write(info, 0, info.Length);
+            }
+            else
+            {
+                File.AppendAllText(path, text);
+                File.AppendAllText(path, Environment.NewLine);
+            }
         }
     }
 }
---
+++ LinqSamples/JobScheduler/JobScheduler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+
+namespace JobScheduler
+{
+    interface IJob
+    {
+        void Execute(DateTime signalTime);
+    }
+
+    public class JobScheduler
+    {
+        private readonly Timer _timer;
+        private readonly List<Action<DateTime>> _actions = new();
+
+        public JobScheduler(int intervalMs)
+        {
+            _timer = new Timer(intervalMs);
+            _timer.Elapsed += OnTimedEvent;
+            _timer.AutoReset = true;
+            _timer.Enabled = false;
+        }
+
+        public void AddHandler(Action<DateTime> action)
+        {
+            _actions.Add(action);
+        }
+
+        public void Start() => _timer.Start();
+        public void Stop() => _timer.Stop();
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs @event)
+        {
+            foreach (var action in _actions)
+            {
+                action?.Invoke(@event.SignalTime);
+            }
+        }
+    }
+}
